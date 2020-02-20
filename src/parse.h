db9ca07957bcf0867967efe1c2f1dd817102f2f3
alex101 <hexuanhua101@gmail.com>
2020-02-20 23:47:37 +0800

    add pre-encode

---
+++ src/parse.h
@@ -0,0 +1,6 @@
+#pragma once
+#include<vector>
+using namespace std;
+void dectobin(unsigned char num, vector<unsigned char> &v);
+unsigned char* parseArray(unsigned char* src,long srcLen);
+unsigned char* inverseParse(unsigned char* src, long srcLen);
