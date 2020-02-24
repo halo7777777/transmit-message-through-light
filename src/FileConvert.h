2e6b52afe74775b212f5e4dd94ebe4e073835859
alex101 <hexuanhua101@gmail.com>
2020-02-25 01:52:44 +0800

    update

---
+++ src/FileConvert.h
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<ctime>
+#include<cstdio>
+#include<Windows.h>
+#include<string>
+using namespace std;
+class FileConvert
+{
+private:
+	FILE* p;
+	unsigned char* dst;
+	unsigned long GetFileSize(FILE* p);
+	void GenerateRandomArray(unsigned char* src, unsigned long size);
+public:
+	~FileConvert();
+	unsigned char* FileToByte(char filename[], unsigned long* size);
+	void ByteToFile(unsigned char* src, char filename[], unsigned long size);
+	void GenerateRandFile(char filename[], unsigned long size);
+	void PicTransToVideo(int picNumPersec);
+	void VideoTransToPic();
+};
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+//#include<iostream>
+//#include"ffmpeg.h"
+//#include"FileConvert.h"
+//using namespace std;
+//int main()
+//{
+//
+//}
