f3d3b959ea019942f40b5b515575833850ee27fc
alex101 <hexuanhua101@gmail.com>
2020-02-25 01:49:46 +0800

    delete

--- src/ffmpeg.h
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-#include<windows.h>
-#include<string>
-#include<stdio.h>
-
-using namespace std;
-void picture_ts_video(int pic_num_persec);//将图片转化为视频，参数：视频帧数
-void video_ts_picture();//将视频转化为图片
