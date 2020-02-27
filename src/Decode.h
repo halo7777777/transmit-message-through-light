e04cd18008dfae3b560305027550728dcb152641
alex101 <hexuanhua101@gmail.com>
2020-02-27 19:15:16 +0800

    add decoder

---
+++ src/Decode.h
@@ -0,0 +1,27 @@
+#pragma once
+#include<opencv2/opencv.hpp>
+#define BEGIN 0
+#define NORMAL 1
+#define END 2
+#define SINGLE 3
+#define MAXSIZE 1054
+
+using namespace std;
+using namespace cv;
+class Decode
+{
+private:
+	const int ROW;
+	const int COL;
+	void rotate(Mat& srcImg, Mat& dst);
+	int  getBit(Vec3b pix);
+
+
+public:
+	Decode(int size=96) :ROW(size), COL(size){}
+	unsigned char* decode(Mat& srcImg, int type, int& length);
+	int getType(Mat& srcImg);
+	int getLength(Mat& srcImg);
+
+};
+
