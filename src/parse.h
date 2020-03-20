d85dc26bd8627ce219dcf49d7e280e5d39fcebf6
alex101 <hexuanhua101@gmail.com>
2020-03-20 20:20:13 +0800

    sort

--- src/parse.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include<vector>
-using namespace std;
-void dectobin(unsigned char num, vector<unsigned char> &v);
-unsigned char* parseArray(unsigned char* src,long srcLen);
-unsigned char* inverseParse(unsigned char* src, long srcLen);
