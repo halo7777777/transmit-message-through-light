2f61e9920a96907314e5dff9bb8b3a909890d5b1
baiyidujiang111 <48401789+baiyidujiang111@users.noreply.github.com>
2020-03-22 08:44:39 +0800

    Add files via upload

---
+++ src/decode/decode_main.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include"Decode.h"
+#include"FileConvert.h"
+#include<cstdio>
+#include<cstdlib>
+//#include"code.h"
+
+#include<algorithm>
+#include"Qrdecode.h"
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+	cout << "This is the decode program." << endl;
+	cout << "Please enter the command" << endl;
+	char takedecode[20];//接受字符串decode
+	char outputname[20];//手机拍摄的视频名称
+	char outfile[20];//输出的二进制文件
+	char outfiletest[20];//输出的校验文件
+	cin >> takedecode >> outputname >> outfile >> outfiletest;
+	Qrdecode mydecode;
+	mydecode.mydecode(outputname,outfile,outfiletest);
+}
