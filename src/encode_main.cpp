0a4fc423c40dd516ff780f65d8ce015c347cdfe4
alex101 <hexuanhua101@gmail.com>
2020-03-23 08:55:31 +0800

    update

--- src/encode_main.cpp
+++ src/encode_main.cpp
@@ -8,17 +8,17 @@
 using namespace std;
 using namespace cv;
 
-int main()
+int main(char argc, char* argv[])
 {
-	cout << "This is the encode program." << endl;
-	cout << "Please enter the command" << endl;
-	char encodetake[20];//用于接受字符串encode
+	//cout << "This is the encode program." << endl;
+	//cout << "Please enter the command" << endl;
+	//char encodetake[20];//用于接受字符串encode
 	char filename[20];//输出二进制文件名称
 	char outvideo[20];//输出视频的名称
 	unsigned int videolength_ms;//视频长度
-	cin >> encodetake >> filename >> outvideo >> videolength_ms;
+	//cin >> encodetake >> filename >> outvideo >> videolength_ms;
 	double times;
-	times = videolength_ms / 1000.0;
+	times = atoi(argv[3]) / 1000.0;
 	unsigned long size;
 	unsigned int fps = 25;
 	unsigned int bitofeachpicture = 1018;
@@ -27,9 +27,9 @@ int main()
 	unsigned char* bytes = NULL;
 
 	FileConvert converter;
-	converter.GenerateRandFile(filename, size);
-	bytes = converter.FileToByte(filename, &size);//读取文件生成的Byte数组
+	converter.GenerateRandFile(argv[1], size);
+	bytes = converter.FileToByte(argv[1], &size);//读取文件生成的Byte数组
 
 	Code::Main(bytes, size, "imageSrc\\", "png", 1);
-	converter.PicTransToVideo(fps,outvideo);
+	converter.PicTransToVideo(fps,argv[2]);
 }
